e5c4532647d5e4de25f0da5a7ca43463f8b12c38
cyrineKc <cyrine.kchir92@gmail.com>
2024-07-04 23:09:00 +0200

    calculate retirement age

--- RetirementCalculator/AppDelegate.swift
+++ RetirementCalculator/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         AppCenter.start(withAppSecret: "0cb77872-e97f-4db1-86d6-09bc9f1d01ba", services: [Analytics.self, Crashes.self])
+        Crashes.hasCrashedInLastSession
         // Override point for customization after application launch.
         return true
     }
--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenter
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monthyInvestTextField: UITextField!
+    @IBOutlet weak var currentAgeTextField: UITextField!
+    @IBOutlet weak var plannedRetiringAgeTextField: UITextField!
+    @IBOutlet weak var avergeInterestTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func calculateButtonTapped(_ sender: Any) {
+        //Crashes.generateTestCrash()
+        let currentAgeText = currentAgeTextField.text!
+        let plannedRetiringAgeText = plannedRetiringAgeTextField.text!
+        let properties = ["current Age" : currentAgeText, "planned retiring age": plannedRetiringAgeText ]
+        Analytics.trackEvent("calculate Button tap", withProperties: properties)
+    }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        Analytics.trackEvent("calculater view appears")
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops" , message: "An Error occured" , preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It is cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 
 }
 
